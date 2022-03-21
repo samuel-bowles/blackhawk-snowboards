777468903bf0237c36cd6827aa5d1ae4de41e6a8
Selwob <66890383+Selwob@users.noreply.github.com>
2022-03-21 16:03:53 +0900

    converted from jQuery to pure js

--- assets/js/3d-viewer_vanilla.js
+++ assets/js/3d-viewer_vanilla.js
@@ -1,38 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    let viewButtons = document.querySelectorAll(".board-viewer-button");
-    function addClick() {
-        viewButtons.forEach(function(element) {
-            element.addEventListener("click", function(event) {
-                openModal();
-                addViewer();
-                let modelSrc = this.href;
-                setSource(modelSrc);
-                event.preventDefault();
-                return false;
-            })
-        })
-    }
+let viewButtons = document.querySelectorAll(".board-viewer-button");
 
-    function openModal() {
+viewButtons.forEach(function(element) {
+    element.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        let modelSrc = this.href;
+
         let pageBody = document.querySelector("body");
-        pageBody.innerHTML += '<div id="bg"></div>';
-        pageBody.innerHTML += '<div id="viewer"></div>';
-        document.querySelector("#bg").addEventListener("click", function(event) {
-            document.querySelector("#bg").remove();
-            document.querySelector("#viewer").remove();
+        pageBody.insertAdjacentHTML("beforeend", `<div id="bg" class="fadeIn"><div id="viewer" class="fadeIn"><model-viewer style="width: 100%; height: 100%" src=${modelSrc} class="fadeIn" auto-rotate camera-controls shadow-intensity="1"></model-viewer></div></div>`)
+
+        document.querySelector("#viewer").addEventListener("click", function(event) {
+            event.stopPropagation();
         })
-    }
 
-    function addViewer() {
-        let viewer = document.querySelector("#viewer");
-        viewer.innerHTML += "<model-viewer style='width: 100%; height: 100%' auto-rotate camera-controls shadow-intensity='1'></model-viewer";
-    }
+        document.querySelector("#bg").addEventListener("click", function() {
+            let bg = document.querySelector("#bg");
+            let viewer = document.querySelector("#viewer");
 
-    function setSource(modelSrc) {
-        let viewer = document.querySelector("model-viewer");
-        viewer.src = modelSrc;
-    }
+            bg.classList.add("fadeOut");
+            viewer.classList.add("fadeOut");
 
-    addClick();
-});
+            setInterval(remove, 500, bg, viewer)
+
+            function remove(elem1, elem2) {
+                elem1.remove();
+                elem2.remove();
+            }
+        })
+    })
+})
+
